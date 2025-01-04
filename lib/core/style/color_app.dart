a928107426faf5fbf744cc99f21d55896d1f59cc
EmanT <tharwateman38@gmail.com>
2025-01-04 16:33:39 +0200

    --

--- lib/core/style/color_app.dart
+++ lib/core/style/color_app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ColorApp {
   static Color colorWhite = Colors.white;
   static Color colorBlack = Colors.black;
-  static Color colorOrange = Color(0xffee7434);
-  static Color colorBlue = Color(0xff203342);
+  static Color colorOrange = const Color(0xffee7434);
+  static Color colorBlue = const Color(0xff203342);
   static Color colorRed = Colors.red;
 }
